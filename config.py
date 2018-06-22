0ba54e853c263fd45df00602cb2e81c1caa1aa26
unknown <rekha.rana@gmail.com>
2018-06-22 12:12:30 -0700

    Submitting Homework

---
+++ config.py
@@ -0,0 +1,13 @@
+# Twitter API Keys
+
+consumer_key = "QNV807KEGuJtwxPTdrZ7UXNyM"
+
+consumer_secret = "bOpL20QynsXS5djfDIdRg4N2T9LIIplJF2SYCj7LfuGPPz4nLC"
+
+access_token = "49968972-odnPCZ9OR8uC75bgP3lSk5rUhsXPMC50crivWmLsu"
+
+access_token_secret = "v7LCSwt0bFDaTZ2KZvaYlyk9IP6vqeisA9p9b0WyHJS05"
+
+
+# OWM API Keys
+OWM_API_key="3daae5b1d67d180cc461b8b0e67730cf"
